c23e9d4fdd4392c19122f871175e860d1ae92062
Mekun-k <akaidonnbei@gmail.com>
2025-02-24 09:04:29 +0900

    feature/section3
    サイドバーのレイアウトをTailwindCSSで実装

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className='bg-customBlue h-full overflow-y-auto px-5 flex flex-col'>
+      <div className='flex-grow'>
+        <div className='cursor-pointer flex justify-evenly items-center border mt-2 rounded-md hover:bg-blue-800 duration-150'>
+          <span className='text-white p-4 text-2xl'>＋</span>
+          <h1 className='text-white text-xl font-semibold p-4'>New Chat</h1>
+        </div>
+        <ul>
+          <li className='cursor-pointer border-b p-4 text-slate-100 hover:bg-slate-700 duration-150'>
+            Room 1
+          </li>
+          <li className='cursor-pointer border-b p-4 text-slate-100 hover:bg-slate-700 duration-150'>
+            Room 2
+          </li>
+          <li className='cursor-pointer border-b p-4 text-slate-100 hover:bg-slate-700 duration-150'>
+            Room 3
+          </li>
+          <li className='cursor-pointer border-b p-4 text-slate-100 hover:bg-slate-700 duration-150'>
+            Room 4
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
