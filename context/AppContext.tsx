e3ad24fddc6e62016a182f3cce911efed5457577
Mekun-k <akaidonnbei@gmail.com>
2025-02-27 08:26:40 +0900

    feature/section3 ユーザーのログイン状態をグローバルに管理

--- context/AppContext.tsx
+++ context/AppContext.tsx
@@ -1,8 +1,17 @@
+import { User } from "firebase/auth";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 import { createContext, ReactNode, useState } from "react";
 
 type AppProviderProps = {
     children: ReactNode;
+};
+
+type AppContextType = {
+    user: User | null;
+    userId: string | null;
+    setUser: React.Dispatch<React.SetStateAction<User | null>>;
+    selectedRoom: string | null;
+    setSelectedRoom: React.Dispatch<React.SetStateAction<string | null>>;
 };
 
 const defalutContextData = {
@@ -11,14 +20,21 @@ const defalutContextData = {
     setUser: () => {},
     selectedRoom: null,
     setSelectedRoom: () => {},
-}
+};
 
-const AppContext = createContext(defalutContextData);
+const AppContext = createContext<AppContextType>(defalutContextData);
 
+// TODO:error発生してるが理解して解決したいのでこのままcommitします
 export function AppProvider({ children }: AppProviderProps) {
-    const [user, setUser] = useState<any | null>(null);
+    const [user, setUser] = useState<User | null>(null);
     const [userId, setUserId] = useState<string | null>(null);
     const [selectedRoom, setSelectedRoom] = useState<string | null>(null);
 
-    return <AppContext.Provider value={{user, userId, setUser, selectedRoom, setSelectedRoom}}><>
+    return(
+        <AppContext.Provider
+            value={{ user, userId, setUser, setSelectedRoom }}
+        >
+            {children}
+        </AppContext.Provider>
+    );
 }
