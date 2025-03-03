8d30d43df68d5873c6ad57655563ca19d81cf51a
Mekun-k <akaidonnbei@gmail.com>
2025-03-04 08:16:36 +0900

    feature/section3
    input欄に入力したメッセージをFireStoreに保存する処理を実装

--- app/components/Chat.tsx
+++ app/components/Chat.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import { FaPaperPlane } from "react-icons/fa";
+import { db } from '../firebase';
+import { addDoc, collection, doc, serverTimestamp } from 'firebase/firestore';
 
 const Chat = () => {
+  const [inputMessage, setInputMessage] = useState<string>("");
+
+  const sendMessage = async () => {
+    if (!inputMessage.trim()) return;
+
+    const messageData = {
+      text: inputMessage,
+      sender: "user",
+      createdAt: serverTimestamp(),
+    };
+
+    // 特定のドキュメントを参照
+    const roomDocRef = doc(db, "rooms", "t5HLhOzz3xceZAkVD4Tp");
+
+    // コレクションを参照
+    const messageCollectionRef = collection(roomDocRef, "messages");
+
+    // コレクションに新しいドキュメントを追加する
+    await addDoc(messageCollectionRef, messageData);
+  }
+
   return (
     <div className='bg-gray-500 h-full p-4 flex flex-col'>
       <h1 className='text-2xl text-white font-semibold mb-4'>Room 1</h1>
@@ -22,9 +47,13 @@ const Chat = () => {
         <input
           type="text"
           placeholder='Send a Message'
-          className='border-2 rounded w-full pr-10 focus:outline-none p-2' 
+          className='border-2 rounded w-full pr-10 focus:outline-none p-2'
+          onChange={(e) => setInputMessage(e.target.value)}
         />
-        <button className='absolute inset-y-0 right-4 flex items-center'>
+        <button 
+          className='absolute inset-y-0 right-4 flex items-center'
+          onClick={() => sendMessage()}
+        >
           <FaPaperPlane />
         </button>
       </div>
